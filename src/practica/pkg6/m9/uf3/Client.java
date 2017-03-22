3b2c6f5f5b0ef512a59e203b0893a35c3262a8d2
davidblancolopez <daviddbl94@gmail.com>
2017-03-22 19:22:40 +0100

    CAMBIOS.

--- src/practica/pkg6/m9/uf3/Client.java
+++ src/practica/pkg6/m9/uf3/Client.java
@@ -1,7 +1,44 @@
-
 package practica.pkg6.m9.uf3;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 
 public class Client {
-    
+
+    private static final int PORT = 5487;
+    private static final String HOST = "231.0.0.8";
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        MulticastSocket socket = new MulticastSocket(PORT);
+        InetAddress inet = InetAddress.getByName(HOST);
+        DatagramPacket dp;
+        String cadena;
+        socket.joinGroup(inet);
+        byte[] missatge1, missatge2;
+        
+        //Bucle que s'executara m'entres que la cadena que rep no sigui exit.
+        do {
+            //Es crea l'array de bytes on ficarem el missatge.    
+            missatge1 = new byte[512];
+            //Creem el datagramPacket que utlitzarem per enviar el missatge.
+            dp = new DatagramPacket(missatge1, missatge1.length);
+            //
+            socket.receive(dp);
+            //Indiquem la llargaria i la guardem en una variable.
+            int mida = dp.getLength();
+            //S'inicialitza l'array on es guardara el missatge amb la mida indicada.
+            missatge2 = new byte[mida];
+            //
+            System.arraycopy(missatge1, 0, missatge2, 0, mida);
+            //Es guarda el missatge.
+            cadena = new String(missatge2);
+            //S'imprimeix el missatge.
+            System.out.println(cadena);
+        } while (!cadena.equals("exit"));
+        socket.leaveGroup(inet);
+    }
+
 }
--- src/practica/pkg6/m9/uf3/Servidor.java
+++ src/practica/pkg6/m9/uf3/Servidor.java
@@ -1,37 +1,43 @@
-
 package practica.pkg6.m9.uf3;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-
+import javax.swing.JOptionPane;
 
 public class Servidor {
+
+    private static final int port = 5487;
+    private static final String host = "231.0.0.8";
+
     MulticastSocket socket;
     InetAddress multicastIP;
-    int port;
     boolean continueRunning = true;
-    
-    public void init(String strIp, int portValue) throws socketException, IOException{
+
+    public void init(String strIp, int portValue) throws IOException {
         socket = new MulticastSocket(portValue);
-        //multicastIP = new InetAddress();
+        MulticastSocket socket = new MulticastSocket(port);
+        InetAddress inet = InetAddress.getByName(host);
+        //Es crea l'array de bytes on ficarem el missatge.
+        byte[] dades = new byte[0];
+        //Creem el datagramPacket que utlitzarem per enviar el missatge.
+        DatagramPacket dp = new DatagramPacket(dades, dades.length, inet, port);
+        String recollirMissatge;
+        
+        //Bucle que s'executara m'entres que la cadena que rep no sigui exit.
+        do {
+            //S'introdueix el missatge que es vol enviar.
+            recollirMissatge = JOptionPane.showInputDialog("Introdueix una cadena:");
+            //Es recolleix el missatge.
+            dades = recollirMissatge.getBytes();
+            //S'afegeix la cadena al datagramPacket que hem creat abans.
+            dp.setData(dades);
+            //Indiquem la llargaria del datagramPacket.
+            dp.setLength(dades.length);
+            //Enviem el missatge.
+            socket.send(dp);
+        } while (!recollirMissatge.equals("exit"));
     }
-    
-    /*
-    socket.joinGroup();
-    
-    
-    while(continueRunning){
-        packet = new DatagramPacket(recieveData, 1024);
-    
-        socket.setSoTimeout(5000);
-    
-        try{
-    
-            socket.recieve(packet);
-    
-    
-    }
-    
-    */
+
 }
